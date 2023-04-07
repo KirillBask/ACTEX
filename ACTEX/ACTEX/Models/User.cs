a249fac4b8b001cf6a8316f9f624629665e5e0b0
KirillBask <kiribaska@gmail.com>
2023-04-08 01:03:35 +0300

    develop - API (Models, UserData, Controller)

---
+++ ACTEX/ACTEX/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace ACTEX.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        [JsonProperty("type_id")]
+        public int TypeId { get; set; }
+        [JsonProperty("last_visit_date")]
+        public DateTime LastVisitDate { get; set; }
+    }
+}
